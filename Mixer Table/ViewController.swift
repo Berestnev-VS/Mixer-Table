0679d87ee11ae8c6c9ebe798a7deb7863baf04db
Berestnev Vladimir <berestnev.sv@gmail.com>
2023-05-11 10:12:15 +0300

    Main code

--- Mixer Table/ViewController.swift
+++ Mixer Table/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var tableView: UITableView!
+    var data: [(Int, Bool)] = []
+    let cellIdentifier = "Cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        data = (1...30).map { ($0, false) }
+        
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        view.addSubview(tableView)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(shuffleData))
+    }
+    
+    @objc func shuffleData() {
+        data.shuffle()
+        tableView.reloadData()
     }
 
+    // MARK: - UITableViewDataSource
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let (number, isChecked) = data[indexPath.row]
+        cell.textLabel?.text = "\(number)"
+        cell.accessoryType = isChecked ? .checkmark : .none
+        return cell
+    }
+
+    // MARK: - UITableViewDelegate
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        var item = data[indexPath.row]
+        item.1.toggle()
+        print(item.0, item.1)
+        data[indexPath.row] = item
+
+        if item.1 {
+            data.remove(at: indexPath.row)
+            data.insert(item, at: 0)
+            
+            tableView.performBatchUpdates({
+                tableView.moveRow(at: indexPath, to: IndexPath(row: 0, section: 0))
+            }) { _ in
+                tableView.reloadRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+            }
+        } else {
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
 
 }
+
 
